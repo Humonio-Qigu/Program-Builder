32fe7040636af28db2ebc8b15aa83b126839b997
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-10-24 14:15:24 +0200

    :heavy_plus_sign: Add dependencies and nuxt modules (with configurations)

---
+++ i18n.config.ts
@@ -0,0 +1,8 @@
+import fr from "~/assets/i18n/fr";
+
+export default defineI18nConfig(() => ({
+  legacy: false,
+  messages: {
+    fr,
+  },
+}));
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    "@nuxtjs/i18n",
+    "@nuxtjs/google-fonts",
+    "shadcn-nuxt",
+  ],
+  devtools: { enabled: true },
+  compatibilityDate: "2024-04-03",
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: {
+        indent: 2,
+        semi: true,
+        quotes: "double",
+      },
+    },
+  },
+  googleFonts: {
+    families: {
+      Montserrat: {
+        wght: "100..900",
+        ital: "100..900",
+      },
+    },
+  },
+  i18n: {
+    vueI18n: "i18n.config.ts",
+    locales: ["fr"],
+    defaultLocale: "fr",
+  },
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.scss", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    viewer: true,
+  },
+});
